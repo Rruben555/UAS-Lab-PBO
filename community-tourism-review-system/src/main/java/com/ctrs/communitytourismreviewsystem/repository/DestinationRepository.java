426ade5b57544abcb9f1172fdb3e51bffe42d692
Rruben555 <rubensiregar25@gmail.com>
2026-06-14 21:32:00 +0700

    Delete community-tourism-review-system directory

--- community-tourism-review-system/src/main/java/com/ctrs/communitytourismreviewsystem/repository/DestinationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ctrs.communitytourismreviewsystem.repository;
-
-import com.ctrs.communitytourismreviewsystem.entity.Destination;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DestinationRepository extends JpaRepository<Destination, Long> {
-}
