33a9538420dea45a5fda5c2bc58036b64237353c
Azkakaban <arrkaban@gmail.com>
2026-06-14 21:34:12 +0700

    Delete community-tourism-review-system directory

--- community-tourism-review-system/src/main/java/com/ctrs/communitytourismreviewsystem/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ctrs.communitytourismreviewsystem.exception;
-
-public class ResourceNotFoundException
-        extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
